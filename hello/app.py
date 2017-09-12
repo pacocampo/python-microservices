7b6e8327c7cefa89d108980a36da22ab20a8cdfd
pacocampo <paco.ocampor@gmail.com>
2017-09-11 21:15:27 -0500

    python request & docker file

---
+++ hello/app.py
@@ -0,0 +1,8 @@
+import webapp2
+from paste import httpserver
+
+class Hello(webapp2.RequestHandler):
+    def get(self):
+         self.response.write("Hello world! :)")
+
+httpserver.serve(webapp2.WSGIApplication([('/', reverse)]), host='0.0.0.0', port='80')
