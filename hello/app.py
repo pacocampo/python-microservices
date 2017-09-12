ff2811410dc8ecf293d6c1ad5ba825fd77188a65
pacocampo <paco.ocampor@gmail.com>
2017-09-11 22:21:24 -0500

    fix on http respons

--- hello/app.py
+++ hello/app.py
@@ -5,4 +5,4 @@ class Hello(webapp2.RequestHandler):
     def get(self):
          self.response.write("Hello world! :)")
 
-httpserver.serve(webapp2.WSGIApplication([('/', reverse)]), host='0.0.0.0', port='80')
+httpserver.serve(webapp2.WSGIApplication([('/', Hello)]), host='0.0.0.0', port='80')
